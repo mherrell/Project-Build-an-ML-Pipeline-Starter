d4fce6ee427c69b772507c2c389784d1faea0a4e
mherrell <mherr@LAPTOP-FSB07L4G.localdomain>
2025-11-19 17:07:22 -0500

    clean data todo

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -34,7 +34,7 @@ def go(args):
     df = df[idx].copy()
     # Save the cleaned file
     df.to_csv('clean_sample.csv',index=False)
-
+ 
     # log the new data.
     artifact = wandb.Artifact(
      args.output_artifact,
